9ebd8a43b552ab12acc41307da96be774f9eafb3
somaye-rajeie <s.rajeee2020@gmail.com>
2025-08-18 13:50:33 +0330

    update responsive header-top

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,11 @@
 // Event Click on menu-mobile
 const navMenu=document.querySelector('.nav-menu')
 const menuHeader=document.querySelector('.header-menu')
+const headerSearch=document.querySelector('.header__search')
+const headerProfile=document.querySelector('.header__profile')
 navMenu.addEventListener('click',function(){
     this.classList.toggle('nav-menu--open')
     menuHeader.classList.toggle('header-menu--mobile')
+    headerSearch.classList.toggle('header-menu--mobile')
+    headerProfile.classList.toggle('header-menu--mobile')
 })
