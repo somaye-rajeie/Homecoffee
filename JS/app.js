3ce460f16417e41635fd1fbd85b9515b9e8c3330
somaye-rajeie <s.rajeee2020@gmail.com>
2025-08-27 11:58:29 +0330

    coding section latest-products & section coffee-brands

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,18 @@ navMenu.addEventListener('click',function(){
     headerSearch.classList.toggle('header-menu--mobile')
     headerProfile.classList.toggle('header-menu--mobile')
 })
+// Event click on latest-products-list__item
+let contentId;
+let contentSelected;
+const latestProducts=document.querySelectorAll('.latest-products-list__item')
+
+latestProducts.forEach(function(item){
+item.addEventListener('click',function(){
+document.querySelector('.active').classList.remove('active')
+document.querySelector('.latest-products__wrapper--show').classList.remove('latest-products__wrapper--show')
+this.classList.add('active')
+contentId=this.getAttribute('data-content-id')
+contentSelected=document.querySelector(contentId)
+contentSelected.classList.add('latest-products__wrapper--show')
+})
+})
